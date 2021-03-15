a89db775158110721c4eec79e4bbd342e60f9bc1
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-03-15 14:29:42 -0400

    1.3: Course Information Step3, adding and refacctor objects

--- Part1/courseinfo/course-info-app/src/App.js
+++ Part1/courseinfo/course-info-app/src/App.js
@@ -11,6 +11,7 @@ const Header = (props) => {
 
 const Content = (props) => {
 // renders the parts and their number of exercises
+console.log('CONTENT', props.exercises1)
   return (
     <div>
       <p> <Part part = {props.part1} exercise = {props.exercises1}> </Part> </p>
@@ -41,24 +42,30 @@ return (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const total = part1.exercises + part2.exercises + part3.exercises
+  
   return (
     <div>
       <h1> <Header course={course}/> </h1>
       <p> <Content 
-          part1={part1} part2={part2} part3={part3} 
-          exercises1={exercises1} exercises2={exercises2}
-          exercises3={exercises3} > 
+          part1={part1.name} part2={part2.name} part3={part3.name} 
+          exercises1={part1.exercises} exercises2={part2.exercises}
+          exercises3={part3.exercises} > 
           </Content>
       </p>
-      <p> <Total total={total}> </Total></p>
+      <p> <Total total={total}></Total></p>
     </div>
   );
 }
