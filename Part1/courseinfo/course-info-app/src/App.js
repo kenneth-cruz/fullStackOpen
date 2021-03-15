e7f88490544d2fe0a191d6cbec16b4618d17018b
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-03-14 23:45:39 -0400

    1.2 course information, step2

--- Part1/courseinfo/course-info-app/src/App.js
+++ Part1/courseinfo/course-info-app/src/App.js
@@ -13,12 +13,22 @@ const Content = (props) => {
 // renders the parts and their number of exercises
   return (
     <div>
-      <p> {props.part1} {props.exercises1} </p>
-      <p> {props.part2} {props.exercises2} </p>
-      <p> {props.part3} {props.exercises3} </p>
+      <p> <Part part = {props.part1} exercise = {props.exercises1}> </Part> </p>
+      <p> <Part part = {props.part2} exercise = {props.exercises2}> </Part> </p>
+      <p> <Part part = {props.part3} exercise = {props.exercises3}> </Part> </p>
     </div>
   )  
 }
+
+const Part = (props) => {
+  return (
+    <div>
+      <p> {props.part} {props.exercise} </p>
+    </div>
+  )
+}
+
+
 
 const Total = (props) => {
 // renders the total number of exercises
